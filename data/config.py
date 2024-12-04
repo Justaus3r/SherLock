43a9b5d802cf1fb94b011f3e2f10ee1f625423af
Justaus3r <x-neron@pm.me>
2024-12-04 16:30:45 +0000

    feat: run tests for 'binary' type

--- data/config.py
+++ data/config.py
@@ -3,15 +3,15 @@ args = {
     'batch_size': 128,
     'num_channels': 3,
     'malnet_tiny': False,
-    'group': 'type',  # options: binary, type, family
+    'group': 'binary',  # options: binary, type, family
     'color_mode': 'rgb',  # options: rgb, grayscale
 
     'seed': 1,
     'devices': [0],
 
-    'groups' : ['family'], # binary, 'type', 'family'
-    'data_dir': 'D:\datasets\malnet\zip\\test',  # symbolic link directory
-    'image_dir': 'D:\datasets\malnet\zip\malnet-images/',  # path where data is located
+    'groups' : ['binary'], # binary, 'type', 'family'
+    'data_dir': '/content/data_dir/',  # symbolic link directory
+    'image_dir': '/content/dataset/malnet-images/',  # path where data is located
     'dataset_type': 'val', # all, train, test, val
     'symbolic': False
 
